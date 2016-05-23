8d4a4b8aee1b60c48243d8f848b75477e1f0ff25
Grzegorz Różycki <grozycki@op.pl>
2016-05-23 23:12:21 +0200

    add test repeat argument, usage comments

--- sh/run-monkey-testsuite.sh
+++ sh/run-monkey-testsuite.sh
@@ -1,25 +1,38 @@
 # run-monkey-testsuite.sh
 # Script runs a whole monkey test suite.
 # Runs all scripts contained in the input directory.
-
+#
+# Usage:
+#   sh run-monkey-testsuite.sh test_dir [test_repeat]
+#
+# Arguments:
+#   test_dir    – Required, directory containing test scenarios
+#
+#   test_repeat – Optional, how many times to repeat a given test
+#
 if [ ! -d "${1}" ]; then
     echo 'Need path to directory containing tests to run'
     exit 1
 fi
 
-if [ ! -d 'bugreports/' ]; then
-    mkdir 'bugreports/'
-
-    if [ $? -ne 0 ]; then
-        echo 'could not create bugreports/ direcotry'
-        exit 2
-    fi
+# how many timest to repeat a given test
+if [ "${2}" -lt 1 ]; then
+    repeat=1
+else
+    repeat=$2
 fi
 
-# wait for user input, so he can disconnect the device
-read -p 'Press RETURN to start tests...' ignore
+dst_dir='bugreports/'
 
+# create destination directory if not present
+mkdir -p "${dst_dir}"
+
+# run tests
 for test in $(ls "${1}"); do
-    echo "Running tests from file ${1}"
-    sh run-monkey-test.sh "${test}"
+    test_path=$(basename "${1}")"/${test}"
+    echo "Running tests from file ${test_path}"
+
+    for i in $(seq $repeat); do
+        sh run-monkey-test.sh "${test_path}" "${dst_dir}"
+    done
 done
