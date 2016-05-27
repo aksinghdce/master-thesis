0f75da819f579ef76792d2ff972997cb68d6ef30
Grzegorz Różycki <grozycki@op.pl>
2016-05-27 14:30:55 +0200

    generated new octave file for icarwash

--- octave/icarwash.m
+++ octave/icarwash.m
@@ -1,4 +1,4 @@
-# Created by conv2m, 2016-05-27 08:43:41
+# Created by conv2m, 2016-05-27 14:25:51
 # name: header
 # type: cell
 # rows: 1
@@ -171,37 +171,37 @@ icarwash/capture_4
 2.007874
 # name: <cell-element>
 # type: scalar
-1.980315
+2.062992
 # name: <cell-element>
 # type: scalar
 0.492126
 # name: <cell-element>
 # type: scalar
-0.43700787
+0.4566929
 # name: <cell-element>
 # type: scalar
 1128193
 # name: <cell-element>
 # type: scalar
-1224156
+1128653
 # name: <cell-element>
 # type: scalar
 1129629
 # name: <cell-element>
 # type: scalar
-1227617
+1142922
 # name: <cell-element>
 # type: scalar
 1089408.9
 # name: <cell-element>
 # type: scalar
-1072849.6
+1120531.8
 # name: <cell-element>
 # type: scalar
 241719.1
 # name: <cell-element>
 # type: scalar
-209119.88
+221411.9
 # name: <cell-element>
 # type: scalar
 2
@@ -219,13 +219,13 @@ icarwash/capture_4
 449378
 # name: <cell-element>
 # type: scalar
-449302
+449250
 # name: <cell-element>
 # type: scalar
 454290
 # name: <cell-element>
 # type: scalar
-454290
+453822
 # name: <cell-element>
 # type: scalar
 106466
@@ -237,55 +237,55 @@ icarwash/capture_4
 99274
 # name: <cell-element>
 # type: scalar
-96020
+95860
 # name: <cell-element>
 # type: scalar
 51
 # name: <cell-element>
 # type: scalar
-50.3
+52.4
 # name: <cell-element>
 # type: scalar
 12.5
 # name: <cell-element>
 # type: scalar
-11.1
+11.6
 # name: <cell-element>
 # type: scalar
 1.0708661
 # name: <cell-element>
 # type: scalar
-1.0708661
+1.0787401
 # name: <cell-element>
 # type: scalar
 0.2515748
 # name: <cell-element>
 # type: scalar
-0.25748032
+0.23779528
 # name: <cell-element>
 # type: scalar
 595227
 # name: <cell-element>
 # type: scalar
-690385
+595595
 # name: <cell-element>
 # type: scalar
 596227
 # name: <cell-element>
 # type: scalar
-692893
+596856
 # name: <cell-element>
 # type: scalar
 580745.4
 # name: <cell-element>
 # type: scalar
-580227.6
+585313
 # name: <cell-element>
 # type: scalar
 121087.945
 # name: <cell-element>
 # type: scalar
-122909.14
+113266.55
 # name: <cell-element>
 # type: scalar
 1
@@ -297,126 +297,126 @@ icarwash/capture_4
 10
 # name: <cell-element>
 # type: scalar
-11
+10
 # name: <cell-element>
 # type: scalar
 168010
 # name: <cell-element>
 # type: scalar
-168062
+168010
 # name: <cell-element>
 # type: scalar
 451170
 # name: <cell-element>
 # type: scalar
-451586
+451170
 # name: <cell-element>
 # type: scalar
 41410
 # name: <cell-element>
 # type: scalar
-40142
+40090
 # name: <cell-element>
 # type: scalar
 96642
 # name: <cell-element>
 # type: scalar
-93956
+93492
 # name: <cell-element>
 # type: scalar
 27.2
 # name: <cell-element>
 # type: scalar
-27.2
+27.4
 # name: <cell-element>
 # type: scalar
 6.39
 # name: <cell-element>
 # type: scalar
-6.54
+6.04
 # name: <cell-element>
 # type: scalar
 0.1472441
 # name: <cell-element>
 # type: scalar
-0.15787403
+0.14685039
 # name: <cell-element>
 # type: scalar
 0.22362205
 # name: <cell-element>
 # type: scalar
-0.22755906
+0.22480315
 # name: <cell-element>
 # type: scalar
 135308
 # name: <cell-element>
 # type: scalar
-230317
+135805
 # name: <cell-element>
 # type: scalar
 135621
 # name: <cell-element>
 # type: scalar
-229356
+135871
 # name: <cell-element>
 # type: scalar
 80161.38
 # name: <cell-element>
 # type: scalar
-85412.91
+79830.08
 # name: <cell-element>
 # type: scalar
 121765.94
 # name: <cell-element>
 # type: scalar
-123044.31
+122228.02
 # name: <cell-element>
 # type: scalar
 10
 # name: <cell-element>
 # type: scalar
-11
+10
 # name: <cell-element>
 # type: scalar
 10
 # name: <cell-element>
 # type: scalar
-11
+10
 # name: <cell-element>
 # type: scalar
 11140
 # name: <cell-element>
 # type: scalar
-11192
+11140
 # name: <cell-element>
 # type: scalar
 451170
 # name: <cell-element>
 # type: scalar
-451378
+451170
 # name: <cell-element>
 # type: scalar
 4638
 # name: <cell-element>
 # type: scalar
-4632
+4528
 # name: <cell-element>
 # type: scalar
 96902
 # name: <cell-element>
 # type: scalar
-93856
+93440
 # name: <cell-element>
 # type: scalar
 3.74
 # name: <cell-element>
 # type: scalar
-4.01
+3.73
 # name: <cell-element>
 # type: scalar
 5.68
 # name: <cell-element>
 # type: scalar
-5.78
+5.71
 
 
