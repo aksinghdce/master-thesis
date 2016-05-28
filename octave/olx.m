cba41068f4628f3a02f63db2ca841ca649a57346
Grzegorz Różycki <grozycki@op.pl>
2016-05-28 14:09:00 +0200

    added analysis results for octave

---
+++ octave/olx.m
@@ -0,0 +1,861 @@
+# Created by conv2m, 2016-05-28 14:07:14
+# name: header
+# type: cell
+# rows: 1
+# columns: 23
+
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 26
+human readable description
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 25
+path to capture directory
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 47
+– series 1 – appStats.DevicePowerPrediction
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 61
+– series 1 – appStats.RawStats.foreground.total_time_msec
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 66
+– series 1 – appStats.RawStats.network.mobile_active_time_msec
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 62
+– series 1 – appStats.RawStats.network.mobile_active_count
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 58
+– series 1 – appStats.RawStats.network.mobile_bytes_rx
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 58
+– series 1 – appStats.RawStats.network.mobile_bytes_tx
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 68
+– series 1 – appStats.RawStats.power_use_item.computed_power_mah
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 47
+– series 2 – appStats.DevicePowerPrediction
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 61
+– series 2 – appStats.RawStats.foreground.total_time_msec
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 66
+– series 2 – appStats.RawStats.network.mobile_active_time_msec
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 62
+– series 2 – appStats.RawStats.network.mobile_active_count
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 58
+– series 2 – appStats.RawStats.network.mobile_bytes_rx
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 58
+– series 2 – appStats.RawStats.network.mobile_bytes_tx
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 68
+– series 2 – appStats.RawStats.power_use_item.computed_power_mah
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 47
+– series 3 – appStats.DevicePowerPrediction
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 61
+– series 3 – appStats.RawStats.foreground.total_time_msec
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 66
+– series 3 – appStats.RawStats.network.mobile_active_time_msec
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 62
+– series 3 – appStats.RawStats.network.mobile_active_count
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 58
+– series 3 – appStats.RawStats.network.mobile_bytes_rx
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 58
+– series 3 – appStats.RawStats.network.mobile_bytes_tx
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 68
+– series 3 – appStats.RawStats.power_use_item.computed_power_mah
+
+
+
+# name: olx
+# type: cell
+# rows: 10
+# columns: 23
+
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 32
+bez optymalizacji, bez kompresji
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 28
+bez optymalizacji, kompresja
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 29
+prefetch 5, potem 1, bez gzip
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 27
+prefetch 5, potem 1, z gzip
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 29
+prefetch 5, potem 2, bez gzip
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 27
+prefetch 5, potem 2, z gzip
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 30
+prefetch 10, potem 1, bez gzip
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 28
+prefetch 10, potem 1, z gzip
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 30
+prefetch 10, potem 2, bez gzip
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 28
+prefetch 10, potem 2, z gzip
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 13
+olx/capture_6
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 13
+olx/capture_1
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 13
+olx/capture_7
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 13
+olx/capture_2
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 13
+olx/capture_8
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 13
+olx/capture_3
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 13
+olx/capture_9
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 13
+olx/capture_4
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 14
+olx/capture_10
+# name: <cell-element>
+# type: string
+# elements: 1
+# length: 13
+olx/capture_5
+# name: <cell-element>
+# type: scalar
+1.9566929
+# name: <cell-element>
+# type: scalar
+1.9370079
+# name: <cell-element>
+# type: scalar
+1.027559
+# name: <cell-element>
+# type: scalar
+1.031496
+# name: <cell-element>
+# type: scalar
+0.96850395
+# name: <cell-element>
+# type: scalar
+0.97637796
+# name: <cell-element>
+# type: scalar
+0.62992126
+# name: <cell-element>
+# type: scalar
+0.6456693
+# name: <cell-element>
+# type: scalar
+0.62598425
+# name: <cell-element>
+# type: scalar
+0.6456693
+# name: <cell-element>
+# type: scalar
+4435731
+# name: <cell-element>
+# type: scalar
+4435795
+# name: <cell-element>
+# type: scalar
+4436158
+# name: <cell-element>
+# type: scalar
+4436530
+# name: <cell-element>
+# type: scalar
+4436520
+# name: <cell-element>
+# type: scalar
+4436232
+# name: <cell-element>
+# type: scalar
+4437233
+# name: <cell-element>
+# type: scalar
+4496141
+# name: <cell-element>
+# type: scalar
+4437281
+# name: <cell-element>
+# type: scalar
+4496249
+# name: <cell-element>
+# type: scalar
+954158
+# name: <cell-element>
+# type: scalar
+942466.06
+# name: <cell-element>
+# type: scalar
+434224.97
+# name: <cell-element>
+# type: scalar
+435350.72
+# name: <cell-element>
+# type: scalar
+399375.97
+# name: <cell-element>
+# type: scalar
+403785.25
+# name: <cell-element>
+# type: scalar
+208141.84
+# name: <cell-element>
+# type: scalar
+209462.66
+# name: <cell-element>
+# type: scalar
+206292.42
+# name: <cell-element>
+# type: scalar
+209093.95
+# name: <cell-element>
+# type: scalar
+89
+# name: <cell-element>
+# type: scalar
+88
+# name: <cell-element>
+# type: scalar
+40
+# name: <cell-element>
+# type: scalar
+40
+# name: <cell-element>
+# type: scalar
+32
+# name: <cell-element>
+# type: scalar
+31
+# name: <cell-element>
+# type: scalar
+20
+# name: <cell-element>
+# type: scalar
+20
+# name: <cell-element>
+# type: scalar
+20
+# name: <cell-element>
+# type: scalar
+20
+# name: <cell-element>
+# type: scalar
+2598126
+# name: <cell-element>
+# type: scalar
+2598178
+# name: <cell-element>
+# type: scalar
+2599234
+# name: <cell-element>
+# type: scalar
+2599026
+# name: <cell-element>
+# type: scalar
+2601018
+# name: <cell-element>
+# type: scalar
+2601746
+# name: <cell-element>
+# type: scalar
+3457894
+# name: <cell-element>
+# type: scalar
+3457998
+# name: <cell-element>
+# type: scalar
+3458050
+# name: <cell-element>
+# type: scalar
+3457686
+# name: <cell-element>
+# type: scalar
+141184
+# name: <cell-element>
+# type: scalar
+140096
+# name: <cell-element>
+# type: scalar
+129912
+# name: <cell-element>
+# type: scalar
+126754
+# name: <cell-element>
+# type: scalar
+131160
+# name: <cell-element>
+# type: scalar
+128114
+# name: <cell-element>
+# type: scalar
+142898
+# name: <cell-element>
+# type: scalar
+144422
+# name: <cell-element>
+# type: scalar
+137058
+# name: <cell-element>
+# type: scalar
+154230
+# name: <cell-element>
+# type: scalar
+49.7
+# name: <cell-element>
+# type: scalar
+49.2
+# name: <cell-element>
+# type: scalar
+26.1
+# name: <cell-element>
+# type: scalar
+26.2
+# name: <cell-element>
+# type: scalar
+24.6
+# name: <cell-element>
+# type: scalar
+24.8
+# name: <cell-element>
+# type: scalar
+16
+# name: <cell-element>
+# type: scalar
+16.4
+# name: <cell-element>
+# type: scalar
+15.9
+# name: <cell-element>
+# type: scalar
+16.4
+# name: <cell-element>
+# type: scalar
+2.066929
+# name: <cell-element>
+# type: scalar
+2.0984251
+# name: <cell-element>
+# type: scalar
+1.7874016
+# name: <cell-element>
+# type: scalar
+1.7401575
+# name: <cell-element>
+# type: scalar
+1.3858268
+# name: <cell-element>
+# type: scalar
+1.3779528
+# name: <cell-element>
+# type: scalar
+0.5866142
+# name: <cell-element>
+# type: scalar
+0.62204725
+# name: <cell-element>
+# type: scalar
+0.6062992
+# name: <cell-element>
+# type: scalar
+0.62204725
+# name: <cell-element>
+# type: scalar
+3459565
+# name: <cell-element>
+# type: scalar
+3459776
+# name: <cell-element>
+# type: scalar
+3460090
+# name: <cell-element>
+# type: scalar
+3460355
+# name: <cell-element>
+# type: scalar
+3460812
+# name: <cell-element>
+# type: scalar
+3460987
+# name: <cell-element>
+# type: scalar
+3461711
+# name: <cell-element>
+# type: scalar
+3458495
+# name: <cell-element>
+# type: scalar
+3461364
+# name: <cell-element>
+# type: scalar
+3459004
+# name: <cell-element>
+# type: scalar
+1020074.6
+# name: <cell-element>
+# type: scalar
+1036995.94
+# name: <cell-element>
+# type: scalar
+861389.44
+# name: <cell-element>
+# type: scalar
+834167.4
+# name: <cell-element>
+# type: scalar
+635297.25
+# name: <cell-element>
+# type: scalar
+631356.5
+# name: <cell-element>
+# type: scalar
+187777.03
+# name: <cell-element>
+# type: scalar
+211088.52
+# name: <cell-element>
+# type: scalar
+200152.98
+# name: <cell-element>
+# type: scalar
+208635.11
+# name: <cell-element>
+# type: scalar
+100
+# name: <cell-element>
+# type: scalar
+99
+# name: <cell-element>
+# type: scalar
+80
+# name: <cell-element>
+# type: scalar
+80
+# name: <cell-element>
+# type: scalar
+60
+# name: <cell-element>
+# type: scalar
+59
+# name: <cell-element>
+# type: scalar
+20
+# name: <cell-element>
+# type: scalar
+20
+# name: <cell-element>
+# type: scalar
+20
+# name: <cell-element>
+# type: scalar
+20
+# name: <cell-element>
+# type: scalar
+3453270
+# name: <cell-element>
+# type: scalar
+3453270
+# name: <cell-element>
+# type: scalar
+3462862
+# name: <cell-element>
+# type: scalar
+3463642
+# name: <cell-element>
+# type: scalar
+3813076
+# name: <cell-element>
+# type: scalar
+3813492
+# name: <cell-element>
+# type: scalar
+3457530
+# name: <cell-element>
+# type: scalar
+3458102
+# name: <cell-element>
+# type: scalar
+3457894
+# name: <cell-element>
+# type: scalar
+3457894
+# name: <cell-element>
+# type: scalar
+170958
+# name: <cell-element>
+# type: scalar
+170122
+# name: <cell-element>
+# type: scalar
+197824
+# name: <cell-element>
+# type: scalar
+192610
+# name: <cell-element>
+# type: scalar
+216300
+# name: <cell-element>
+# type: scalar
+214630
+# name: <cell-element>
+# type: scalar
+137718
+# name: <cell-element>
+# type: scalar
+143874
+# name: <cell-element>
+# type: scalar
+135982
+# name: <cell-element>
+# type: scalar
+137066
+# name: <cell-element>
+# type: scalar
+52.5
+# name: <cell-element>
+# type: scalar
+53.3
+# name: <cell-element>
+# type: scalar
+45.4
+# name: <cell-element>
+# type: scalar
+44.2
+# name: <cell-element>
+# type: scalar
+35.2
+# name: <cell-element>
+# type: scalar
+35
+# name: <cell-element>
+# type: scalar
+14.9
+# name: <cell-element>
+# type: scalar
+15.8
+# name: <cell-element>
+# type: scalar
+15.4
+# name: <cell-element>
+# type: scalar
+15.8
+# name: <cell-element>
+# type: scalar
+2.519685
+# name: <cell-element>
+# type: scalar
+2.5472438
+# name: <cell-element>
+# type: scalar
+2.1141732
+# name: <cell-element>
+# type: scalar
+2.0787401
+# name: <cell-element>
+# type: scalar
+1.4724411
+# name: <cell-element>
+# type: scalar
+1.4370079
+# name: <cell-element>
+# type: scalar
+1.6850394
+# name: <cell-element>
+# type: scalar
+1.6811024
+# name: <cell-element>
+# type: scalar
+1.4488189
+# name: <cell-element>
+# type: scalar
+1.496063
+# name: <cell-element>
+# type: scalar
+1659328
+# name: <cell-element>
+# type: scalar
+1659558
+# name: <cell-element>
+# type: scalar
+1666383
+# name: <cell-element>
+# type: scalar
+1660537
+# name: <cell-element>
+# type: scalar
+1660829
+# name: <cell-element>
+# type: scalar
+1661095
+# name: <cell-element>
+# type: scalar
+1661113
+# name: <cell-element>
+# type: scalar
+1659499
+# name: <cell-element>
+# type: scalar
+1660455
+# name: <cell-element>
+# type: scalar
+1653732
+# name: <cell-element>
+# type: scalar
+1304313
+# name: <cell-element>
+# type: scalar
+1319402
+# name: <cell-element>
+# type: scalar
+1071923.5
+# name: <cell-element>
+# type: scalar
+1055048.5
+# name: <cell-element>
+# type: scalar
+716511
+# name: <cell-element>
+# type: scalar
+696660.8
+# name: <cell-element>
+# type: scalar
+833339.3
+# name: <cell-element>
+# type: scalar
+833664.5
+# name: <cell-element>
+# type: scalar
+703010.25
+# name: <cell-element>
+# type: scalar
+729847.3
+# name: <cell-element>
+# type: scalar
+61
+# name: <cell-element>
+# type: scalar
+62
+# name: <cell-element>
+# type: scalar
+44
+# name: <cell-element>
+# type: scalar
+41
+# name: <cell-element>
+# type: scalar
+46
+# name: <cell-element>
+# type: scalar
+43
+# name: <cell-element>
+# type: scalar
+53
+# name: <cell-element>
+# type: scalar
+51
+# name: <cell-element>
+# type: scalar
+53
+# name: <cell-element>
+# type: scalar
+50
+# name: <cell-element>
+# type: scalar
+8818670
+# name: <cell-element>
+# type: scalar
+8740750
+# name: <cell-element>
+# type: scalar
+8980083
+# name: <cell-element>
+# type: scalar
+8743830
+# name: <cell-element>
+# type: scalar
+8742670
+# name: <cell-element>
+# type: scalar
+8742670
+# name: <cell-element>
+# type: scalar
+8747674
+# name: <cell-element>
+# type: scalar
+8747310
+# name: <cell-element>
+# type: scalar
+9137898
+# name: <cell-element>
+# type: scalar
+9060546
+# name: <cell-element>
+# type: scalar
+433138
+# name: <cell-element>
+# type: scalar
+426766
+# name: <cell-element>
+# type: scalar
+446939
+# name: <cell-element>
+# type: scalar
+430600
+# name: <cell-element>
+# type: scalar
+425284
+# name: <cell-element>
+# type: scalar
+420558
+# name: <cell-element>
+# type: scalar
+427848
+# name: <cell-element>
+# type: scalar
+421506
+# name: <cell-element>
+# type: scalar
+434122
+# name: <cell-element>
+# type: scalar
+439278
+# name: <cell-element>
+# type: scalar
+64
+# name: <cell-element>
+# type: scalar
+64.7
+# name: <cell-element>
+# type: scalar
+53.7
+# name: <cell-element>
+# type: scalar
+52.8
+# name: <cell-element>
+# type: scalar
+37.4
+# name: <cell-element>
+# type: scalar
+36.5
+# name: <cell-element>
+# type: scalar
+42.8
+# name: <cell-element>
+# type: scalar
+42.7
+# name: <cell-element>
+# type: scalar
+36.8
+# name: <cell-element>
+# type: scalar
+38
+
+
